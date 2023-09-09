9708ce7a4c6f0912c2fc82d044f4e447bbf82b1e
Emmanoel Lucas Bueno <131723445+Emlsbb@users.noreply.github.com>
2023-09-09 18:32:55 -0300

    Pequenos ajustes

--- src/database/index.js
+++ src/database/index.js
@@ -5,19 +5,10 @@ const database = new Sequelize(configDB);
 
 
 //Requirindo os modelos
-const {MerendeiraModel} = require ('../models/merendeira-model');
-const {EstoqueDiarioModel} = require ('../models/EstoqueDiario-model');
-const {UserModel} = require ('../models/user-model');
 const {ServidorModel} = require ('../models/servidor-model')
 
-//inicializando eles
-MerendeiraModel.init(database);
-EstoqueDiarioModel.init(database);
-UserModel.init(database);
+//inicializando modelos para integração com banco
 ServidorModel.init(database)
-
-// Inicie as relações aqui
-
 
 
 module.exports = database
--- src/routes.js
+++ src/routes.js
@@ -9,9 +9,9 @@ const routes = Router();
 
 //Servidores
 routes.post('/servidor', ServidorController.create); //rota funcionando
-routes.get('/merendeira', ServidorController.findAll);
-routes.put('/merendeiraid', ServidorController.update);
-routes.delete('/merendeiraid', ServidorController.delete);
+routes.get('/servidor', ServidorController.findAll);
+routes.put('/servidor:id', ServidorController.update);
+routes.delete('/servidor:id', ServidorController.delete);
 
 //Exportação de rotas
 module.exports = {routes};
